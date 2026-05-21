d95d50b94dd54cb408e5abfa2149d432b73b2734
xinmotlanthua <275663218+xinmotlanthua@users.noreply.github.com>
2026-05-21 11:36:44 +0700

    refactor(handlers): replace handlers.ts (890 LoC) with handlers/ folder

---
+++ src/stamps/geometry-2d/editor/handlers/index.ts
@@ -0,0 +1,5 @@
+export { handleDown } from './pointerDown';
+export { handleMove } from './pointerMove';
+export { handleUp } from './pointerUp';
+export { finalizeTransform } from './transform';
+export type { HandlerCtx, TransformToolKey } from './ctx';
